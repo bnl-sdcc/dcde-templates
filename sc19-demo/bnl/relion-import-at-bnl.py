89cecc0d747b04d73f3d6822a652534ae8fc332a
David Cowley <david.cowley@pnnl.gov>
2019-11-13 21:56:30 -0800

    create BNL relion import script

---
+++ sc19-demo/bnl/relion-import-at-bnl.py
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+parsl.load(bnl_config)
+bnl_dfk = parsl.dfk()
+print(bnl_dfk)
+
+@bash_app
+def relion_import(job_dir=None, stdout=None, stderr=None, mock=True):
+    """
+    Parameters
+    ----------
+    mock : (Bool)
+       when mock=True
+    """
+    cmd_line = '''#!/bin/bash -l
+
+    FIX ALL THIS: THESE ARE ARGONNE PATHS
+
+export DATAROOT=/hpcgpfs01/scratch/dcde1000006/sc19-data
+export RELION_SIMG=/lcrc/project/DCDE/relion/relion_singv26.simg
+
+export INSTAR=${{DATAROOT}}/CtfFind/job003/micrographs_ctf.star
+export REFSTAR=${{DATAROOT}}/Select/job007/class_averages.star
+export PICKDIR=${{DATAROOT}}/AutoPick/job010/
+
+echo -n "working directory: "
+pwd
+set -v
+
+singularity exec  -B /hpcgpfs01:/hpcgpfs01 ${HOME}/relion_singv26.simg relion_star_loopheader rlnMicrographMovieName > $MOVIESTAR
+singularity exec  -B /hpcgpfs01:/hpcgpfs01 ${HOME}/relion_singv26.simg ls Micrographs/*.mrcs >> $MOVIESTAR
+    '''
+    if mock:
+        return '''tmp_file=$(mktemp);
+cat<<EOF > $tmp_file
+{}
+EOF
+cat $tmp_file
+        '''.format(cmd_line)
+    else:
+        return cmd_line
+
+
+
+#relion_stdout='/blues/gpfs/home/dcowley/relion-bootstrap/relion-bnl-import.out'
+#relion_stderr='/blues/gpfs/home/dcowley/relion-bootstrap/relion-bnl-import.err'
+relion_stdout=os.path.join(bnl_config.working_dir, 'relion-bnl-import.out')
+relion_stderr=os.path.join(bnl_config.working_dir, 'relion-bnl-import.err')
+
+"""
+try:
+    os.remove(relion_stdout)
+except OSError:
+    pass
+except FileNotFoundError:
+    pass
+try:
+    os.remove(relion_stderr)
+except OSError:
+    pass
+except FileNotFoundError:
+    pass
+"""
+
+
+print ('job setup: stdout = {}\nstderr = {}'.format(relion_stdout,relion_stderr))
+# parsl.set_stream_logger()
+
+x = relion_import(job_dir=bnl_config.working_dir, stdout=relion_stdout, stderr=relion_stderr, mock = True)
+print('relion_import() invoked, now wating...')
+x.result()
+
+if x.done():
+    bnl_dfk.executors['anl-slurm'].provider.channel.pull_file(relion_stdout, '.')
+    #with open(x.stdout, 'r') as f:
+    #with open(relion_stdout, 'r') as f:
+    # FIXME: fix path handling here:
+    #with open('relion-bnl-import.out', 'r') as f:
+    with open('relion_stdout', 'r') as f:
+        print(f.read())
